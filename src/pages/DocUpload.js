446c7a5fbe81d4722e3ef9bbaf27fc1d192da8d0
xaviSalazar <xavicoel@gmail.com>
2023-08-30 11:34:27 +0200

    Fix: Files saved correctly

--- src/pages/DocUpload.js
+++ src/pages/DocUpload.js
@@ -124,7 +124,7 @@ export default function UserPage() {
       files.some((file) => {
           if (uploaded.findIndex((f) => f.name === file.name) === -1) {
              
-              uploaded.push(file);
+              // uploaded.push(file);
               if (uploaded.length === MAX_COUNT) setFileLimit(true);
               if (uploaded.length > MAX_COUNT) {
                   alert(`You can only add a maximum of ${MAX_COUNT} files`);
@@ -157,11 +157,15 @@ export default function UserPage() {
       });
 
       let res = await Promise.all(filesAsync)
-      
+
+      res.some((file) => {
+      uploaded.push(file)
+      })
+
       if (!limitExceeded) 
       {
         setUploadedFiles(uploaded)
-        dispatch(saveFiles(res));
+        dispatch(saveFiles(uploaded));
       }
 
   }
