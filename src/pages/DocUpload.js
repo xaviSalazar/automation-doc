4dc8470234c706118f77f724a553d2a7e55a2a89
xaviSalazar <xavicoel@gmail.com>
2023-08-30 15:20:36 +0200

    FIX: not to upload existing files

--- src/pages/DocUpload.js
+++ src/pages/DocUpload.js
@@ -131,12 +131,13 @@ export default function UserPage() {
 
     const handleUploadFiles = async files => {
 
+      let handledItems = [];
       const uploaded = [...uploadedFiles];
       let limitExceeded = false;
       files.some((file) => {
-          if (uploaded.findIndex((f) => f.name === file.name) === -1) {
-             
-              // uploaded.push(file);
+          if (!uploaded.some((f) => f.name === file.name)) {
+              console.log('file: ', file)
+              handledItems.push(file);
               if (uploaded.length === MAX_COUNT) setFileLimit(true);
               if (uploaded.length > MAX_COUNT) {
                   alert(`You can only add a maximum of ${MAX_COUNT} files`);
@@ -145,11 +146,12 @@ export default function UserPage() {
                   return true;
               }
           }
-
+          return false;
       })
 
+      console.log('files', handledItems)
        // Convert the FileList into an array and iterate
-       let filesAsync = Array.from(files).map(file => {    
+       let filesAsync = Array.from(handledItems).map(file => {    
         // Define a new file reader
         let reader = new FileReader();
         // Create a new promise
