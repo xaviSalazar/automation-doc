7dc14abc1cd14a29297d58cefa8b3a75e6e62157
xaviSalazar <xavicoel@gmail.com>
2024-02-07 14:26:20 +0100

    Feat: Add erase file button

--- src/pages/libs/ChatLayout.js
+++ src/pages/libs/ChatLayout.js
@@ -16,18 +16,23 @@ import {
 } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'; // Import check circle icon
+import DeleteIcon from '@mui/icons-material/Delete'; // Import delete icon
 
- const mensajes = []
+
+
+const mensajes = []
 
 export default function ChatLayout() {
 
   const [inputValue, setInputValue] = useState('');
   const [messages, setMessages] = useState(mensajes);
   const [inputPosition, setInputPosition] = useState('top');
+  const [fileUploaded, setFileUploaded] = useState(false);
   const [highlight, setHighlight] = useState(false);
   const messagesContainerRef = useRef(null);
   const [page, setPage] = useState(0);
-  const { selectedChatId, conversationArr, 
+  const { selectedChatId, conversationArr,
     isLoadingMessage, hasMore, chatAnswer,
     appendHistory } = useSelector(state => state.conversationHistory)
 
@@ -42,7 +47,7 @@ export default function ChatLayout() {
 
 
   useEffect(() => {
-    if(chatAnswer === '') return;
+    if (chatAnswer === '') return;
     console.log(chatAnswer)
     dispatch(cleanReceivedMsg())
     setMessages(prevMessages => [...prevMessages, chatAnswer]);
@@ -50,11 +55,11 @@ export default function ChatLayout() {
 
 
   useEffect(() => {
-    if(selectedChatId === '') return;
+    if (selectedChatId === '') return;
     const pagina = 0;
     setPage(0);
     dispatch(loadHistory(userCard['id'], selectedChatId, pagina, 10));
-  }, [selectedChatId,dispatch]);
+  }, [selectedChatId, dispatch]);
 
   useEffect(() => {
     if (page > 0) {  // Avoid running on initial render
@@ -64,7 +69,7 @@ export default function ChatLayout() {
   }, [dispatch, userCard, page, selectedChatId]);
 
   useEffect(() => {
-    setMessages(prevMessages => [ ...appendHistory, ...prevMessages]);
+    setMessages(prevMessages => [...appendHistory, ...prevMessages]);
   }, [appendHistory])
 
   // ONLY LOADS FIRST RENDER
@@ -74,22 +79,22 @@ export default function ChatLayout() {
 
   useEffect(() => {
     const handleScroll = () => {
-      const { scrollTop  } = messagesContainerRef.current;
+      const { scrollTop } = messagesContainerRef.current;
       const nearingTop = scrollTop === 0;
       if (nearingTop && hasMore) {
         console.log('useEffect 5')
         setPage(prevPage => prevPage + 1);
       }
     };
-  
+
     const messagesContainer = messagesContainerRef.current;
     messagesContainer.addEventListener('scroll', handleScroll);
-    
+
     return () => {
       messagesContainer.removeEventListener('scroll', handleScroll);
     };
   }, [hasMore]);
-  
+
 
   async function processStream(reader, decoder) {
     const { value, done } = await reader.read();
@@ -99,68 +104,72 @@ export default function ChatLayout() {
     }
     const decodedChunk = decoder.decode(value, { stream: true });
     // Update the messages state by appending the new chunk to the last message
-  setMessages(prevMessages => {
-    if (prevMessages.length === 0 || prevMessages[prevMessages.length - 1].role !== 'assistant') {
-      // If there are no messages or the last message is not from the server, add a new message
-      return [...prevMessages, { content: decodedChunk, role: 'assistant' }];
-    } else {
-      // Otherwise, update the last message with the new chunk
-      const updatedMessages = [...prevMessages];
-      updatedMessages[updatedMessages.length - 1] = {
-        ...updatedMessages[updatedMessages.length - 1],
-        content: updatedMessages[updatedMessages.length - 1].content + decodedChunk,
-      };
-      return updatedMessages;
-    }
-  });
+    setMessages(prevMessages => {
+      if (prevMessages.length === 0 || prevMessages[prevMessages.length - 1].role !== 'assistant') {
+        // If there are no messages or the last message is not from the server, add a new message
+        return [...prevMessages, { content: decodedChunk, role: 'assistant' }];
+      } else {
+        // Otherwise, update the last message with the new chunk
+        const updatedMessages = [...prevMessages];
+        updatedMessages[updatedMessages.length - 1] = {
+          ...updatedMessages[updatedMessages.length - 1],
+          content: updatedMessages[updatedMessages.length - 1].content + decodedChunk,
+        };
+        return updatedMessages;
+      }
+    });
 
     // Call the function recursively to read the next chunk
     await processStream(reader, decoder);
   }
-  
+
   const handleSendMessage = async () => {
-     try { 
-          if (inputValue.trim() !== '') 
-          {
-                    const msgSchema = {
-                      role: 'user',
-                      content: inputValue,
-                      senderId: userCard['id'],
-                      receiverId: "b5fcfbd7-52ba-4786-bea0-d74ed1dbf589",
-                      conversationId: selectedChatId
-                    }
-
-                  setMessages(prevMessages => [...prevMessages, msgSchema]);
-                  setInputValue('');
-
-                const response = await httpManager.streamingResponseConversation(JSON.stringify(msgSchema))
-            
-                if (!response.ok || !response.body) 
-                {
-                  throw response.statusText;
-                }
-                const reader = response.body.getReader();
-                const decoder = new TextDecoder();
-
-              // Start processing the stream
-              await processStream(reader, decoder);
-          }
-        } catch(error) {
-           console.log(error.message)
+    try {
+      if (inputValue.trim() !== '') {
+        const msgSchema = {
+          role: 'user',
+          content: inputValue,
+          senderId: userCard['id'],
+          receiverId: "b5fcfbd7-52ba-4786-bea0-d74ed1dbf589",
+          conversationId: selectedChatId
         }
+
+        setMessages(prevMessages => [...prevMessages, msgSchema]);
+        setInputValue('');
+
+        const response = await httpManager.streamingResponseConversation(JSON.stringify(msgSchema))
+
+        if (!response.ok || !response.body) {
+          throw response.statusText;
+        }
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder();
+
+        // Start processing the stream
+        await processStream(reader, decoder);
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
   };
 
-  const handleFileEvent =  async (e) => {
-    
+  const handleFileEvent = async (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
-    reader.onloadend = () =>  { 
-      const base64Image = reader.result;  
-      console.log(base64Image)
+
+    reader.onloadend = () => {
+      const base64Image = reader.result;
+      setFileUploaded(true);
     }
 
-    if (file) 
-    { reader.readAsDataURL(file); } 
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDelete = () => {
+    setFileUploaded(false);
+    document.getElementById('fileUpload').value = '';
   };
 
   const handleInputClick = () => {
@@ -181,107 +190,112 @@ export default function ChatLayout() {
 
   return (
     <>
-            <List 
-              ref={messagesContainerRef}
-              sx={{    
-                  flexgrow: 1,
-                  overflowY: 'auto',
-                  mb: 2,
-                  }}>
-              {messages.map((message, index) => (
+      <List
+        ref={messagesContainerRef}
+        sx={{
+          flexgrow: 1,
+          overflowY: 'auto',
+          mb: 2,
+        }}>
+        {messages.map((message, index) => (
 
-                <ListItem
-                  key={index}
-                  disableGutters
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: message.role === 'user' ? 'flex-end' : 'flex-start',
-                  }}
-                >
-                  <ListItemText
-                    primary={message.content}
+          <ListItem
+            key={index}
+            disableGutters
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: message.role === 'user' ? 'flex-end' : 'flex-start',
+            }}
+          >
+            <ListItemText
+              primary={message.content}
+              sx={{
+                backgroundColor: message.role === 'user' ? '#DCF8C6' : '#E3F2FD',
+                padding: '8px',
+                borderRadius: '8px',
+                maxWidth: '75%',
+                marginBottom: '4px',
+              }}
+            />
+            {isLoadingMessage && index === messages.length - 1 && (
+              <CircularProgress size={40} color="inherit" sx={{ alignSelf: 'flex-start', marginRight: 2 }} />
+            )}
+            {message.buttons && (
+              <ButtonGroup>
+                {message.buttons.map((button, buttonIndex) => (
+                  <Button
+                    key={buttonIndex}
+                    onClick={button.onClick}
                     sx={{
                       backgroundColor: message.role === 'user' ? '#DCF8C6' : '#E3F2FD',
-                      padding: '8px',
-                      borderRadius: '8px',
-                      maxWidth: '75%',
-                      marginBottom: '4px',
+                      '&:hover': {
+                        backgroundColor: message.role === 'user' ? '#B7E69E' : '#90CAF9',
+                      },
                     }}
-                  />
-                  {isLoadingMessage && index === messages.length - 1 && (
-                    <CircularProgress size={40} color="inherit" sx={{ alignSelf: 'flex-start', marginRight: 2 }} />
-                  )}
-                  {message.buttons && (
-                    <ButtonGroup>
-                      {message.buttons.map((button, buttonIndex) => (
-                        <Button
-                          key={buttonIndex}
-                          onClick={button.onClick}
-                          sx={{
-                            backgroundColor: message.role === 'user' ? '#DCF8C6' : '#E3F2FD',
-                            '&:hover': {
-                              backgroundColor: message.role === 'user' ? '#B7E69E' : '#90CAF9',
-                            },
-                          }}
-                        >
-                          {button.label}
-                        </Button>
-                      ))}
-                    </ButtonGroup>
-                  )}
-                </ListItem>
-              ))}
-            </List>
+                  >
+                    {button.label}
+                  </Button>
+                ))}
+              </ButtonGroup>
+            )}
+          </ListItem>
+        ))}
+      </List>
 
-            <Paper
-              component="form"
-              sx={{
-                p: '2px 4px',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between', // Add this to evenly distribute items horizontally
-                boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-                backgroundColor: highlight ? '#ccc' : 'transparent', // Highlight background color
-                transition: 'background-color 0.5s ease-in-out', // Add a smooth transition effect
-                mt: 'auto',
-              }}
-              onClick={handleInputClick}
-            >
-              <IconButton variant="contained" component="label">
-              <AttachFileIcon />
-                <input
-                            id     = "fileUpload"
-                            type   = "file"
-                            // multiple
-                            accept = ".jpg, .jpeg, .png, .gif"
-                            hidden
-                            onChange={(e) => handleFileEvent(e)}
-                            //disabled={fileLimit}
-                 />
-            </IconButton>
-              {isLoadingMessage ? (
-                <CircularProgress size={24} /> // Show loading icon
-              ) : (<InputBase
-                sx={{ ml: 1, flex: 1, width: 700, maxHeight: 100, overflowY:'auto' }}
-                onChange={handleInputChange}
-                placeholder="Empieza por un Hola chatgpt como estas..."
-                value={inputValue}
-                inputProps={{ 'aria-label': 'chat message input' }}
-                multiline
-              />
-              )}
-              <IconButton
-                onClick={handleSendMessage}
-                color="primary"
-                sx={{ p: '10px' }}
-                aria-label="send message"
-              >
-                <SendIcon />
-              </IconButton>
-            </Paper>
+      <Paper
+        component="form"
+        sx={{
+          p: '2px 4px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between', // Add this to evenly distribute items horizontally
+          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
+          backgroundColor: highlight ? '#ccc' : 'transparent', // Highlight background color
+          transition: 'background-color 0.5s ease-in-out', // Add a smooth transition effect
+          mt: 'auto',
+        }}
+        onClick={handleInputClick}
+      >
+        <IconButton variant="contained" component="label">
+          {fileUploaded ? <CheckCircleIcon /> : <AttachFileIcon />}
+          <input
+            id="fileUpload"
+            type="file"
+            // multiple
+            accept=".jpg, .jpeg, .png, .gif"
+            hidden
+            onChange={(e) => handleFileEvent(e, setFileUploaded)}
+          //disabled={fileLimit}
+          />
+        </IconButton>
+        {fileUploaded && (
+          <IconButton onClick={handleDelete} aria-label="delete uploaded file">
+            <DeleteIcon />
+          </IconButton>
+        )}
+        {isLoadingMessage ? (
+          <CircularProgress size={24} /> // Show loading icon
+        ) : (<InputBase
+          sx={{ ml: 1, flex: 1, width: 700, maxHeight: 100, overflowY: 'auto' }}
+          onChange={handleInputChange}
+          placeholder="Empieza por un Hola chatgpt como estas..."
+          value={inputValue}
+          inputProps={{ 'aria-label': 'chat message input' }}
+          multiline
+        />
+        )}
+        <IconButton
+          onClick={handleSendMessage}
+          color="primary"
+          sx={{ p: '10px' }}
+          aria-label="send message"
+        >
+          <SendIcon />
+        </IconButton>
+      </Paper>
 
-            </>
+    </>
 
   );
 };
